4fa96b7d91eebb4f6231fc29df60fc5c1b9d3c35
mmcgarg <mmcgarg@gmail.com>
2020-04-04 19:57:15 +0530

    added log4j and test class for Problem1

--- src/main/java/leetcode/Problem1TwoSum.java
+++ src/main/java/leetcode/Problem1TwoSum.java
@@ -1,10 +1,22 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author mmcgarg
+ *
+ *         Problem's details: https://leetcode.com/problems/two-sum/
+ *
+ */
 public class Problem1TwoSum {
-	public int[] twoSum(int[] nums, int target) {
+	private static final Logger logger = LogManager.getLogger(Problem1TwoSum.class);
+
+	public static int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> complimentValues = new HashMap<>();
 
 		for (int i = 0; i < nums.length; i++) {
@@ -12,9 +24,18 @@ public class Problem1TwoSum {
 				return new int[] { complimentValues.get(nums[i]), i };
 			}
 			complimentValues.put(target - nums[i], i);
-
 		}
 
-		return null;
+		return new int[0];
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+
+		int[] twoSum = Problem1TwoSum.twoSum(nums, target);
+
+		logger.info(Arrays.toString(twoSum));
+
 	}
 }
