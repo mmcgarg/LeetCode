abf64ed711158cb4912c560678839a05af4ccfe6
mmcgarg <mmcgarg@gmail.com>
2020-04-10 16:46:33 +0530

    Cleaner Problem21

--- src/main/java/leetcode/Problem0021MergeTwoSortedLists.java
+++ src/main/java/leetcode/Problem0021MergeTwoSortedLists.java
@@ -18,45 +18,28 @@ class ListNode {
 public class Problem0021MergeTwoSortedLists {
 
 	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-		if (l2 == null) {
-			return l1;
-		} else if (l1 == null) {
-			return l2;
-		}
+		ListNode temp = null;
+		ListNode result = null;
+		temp = result = new ListNode(-1);
 
-		ListNode l1Temp = l1.val > l2.val ? l2 : l1;
-
-		while (l1 != null) {
-			ListNode l1Next = l1.next;
-			boolean move = true;
-			while (l2 != null) {
-				ListNode l2Next = l2.next;
-
-				if (l1.val <= l2.val && (l1.next == null || l1.next.val >= l2.val)) {
-					l1.next = l2;
-					l2.next = l1Next;
-					l2 = l2Next;
-					move = false;
-					break;
-				} else if (l1.val > l2.val) {
-					l2.next = l1;
-					l1 = l2;
-					l2 = l2Next;
-					move = false;
-					break;
-				} else {
-					break;
-				}
-			}
-			if (move)
+		while (l1 != null && l2 != null) {
+			if (l1.val <= l2.val) {
+				temp.next = l1;
 				l1 = l1.next;
+			} else if (l1.val > l2.val) {
+				temp.next = l2;
+				l2 = l2.next;
+			}
+			temp = temp.next;
 		}
 
-		if (l2 == null) {
-			return l1Temp;
-		} else {
-			return null;
+		if (l1 != null) {
+			temp.next = l1;
+		} else if (l2 != null) {
+			temp.next = l2;
 		}
+
+		return result.next;
 	}
 
 }
